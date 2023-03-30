1723cb1357bac7609ef7525b5dc6144b873ce8a2
Dennis Musembi <dennismusembi344@gmail.com>
2023-03-30 15:32:50 +0300

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your code here
+function submitData(name,email){
+    return fetch("http://localhost:3000/users",{
+        method:'POST',
+        headers:{
+            'Content-Type': 'application/json',
+            'Accept':'application/json'
+        },
+        body:JSON.stringify({name,email})
+    })
+    .then(res=>res.json())
+    .then(object=>{
+        let pElement = document.createElement('p');
+      pElement.textContent = object.id;
+      document.body.appendChild(pElement);
+    })
+    .catch(error=>{
+        alert("Si kuzuri BOSS!")
+        document.body.innerHTML = error.message
+    })
+}
